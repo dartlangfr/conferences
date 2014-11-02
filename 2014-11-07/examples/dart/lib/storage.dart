8f6d142cc6e2cdba6667dec9a438e0075162df60
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2014-11-02 16:37:57 +0100

    more storage

--- 2014-11-07/examples/dart/lib/storage.dart
+++ 2014-11-07/examples/dart/lib/storage.dart
@@ -1,13 +1,28 @@
 import 'package:gcloud/storage.dart';
 
+
+
+void listBuckets(){
+  String project = Platform.environment[PROJECT_ENV];
+  String serviceKeyLocation = Platform.environment[SERVICE_KEY_LOCATION_ENV];
+  final Storage storage = new Storage(client, project);
+  storage.listBucketNames().listen(print);
+}
+
 void helloWorldBucket(Storage storage, String bucketName){
   storage.bucketExists(bucketName).then((exist){
       if(!exist) {
         storage.createBucket('myBucket').then((Bucket bucket) {
           print('bucket ${bucket.bucketName} is created');
           bucket.write("Hello world");
+          bucket.
         });
       }
-    });
+  });
+  storage.
+}
+
+void helloACL(Storage storage, Bucket bucket){
+  storage.bucketI
 }
 
