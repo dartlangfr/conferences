074e0ddb309c5de52abbcdfeb7d57c8d5055e370
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2014-11-02 15:55:38 +0100

    storage first sample

--- 2014-11-07/examples/dart/lib/storage.dart
+++ 2014-11-07/examples/dart/lib/storage.dart
@@ -1,4 +1,13 @@
 import 'package:gcloud/storage.dart';
 
-
+void helloWorldBucket(Storage storage, String bucketName){
+  storage.bucketExists(bucketName).then((exist){
+      if(!exist) {
+        storage.createBucket('myBucket').then((Bucket bucket) {
+          print('bucket ${bucket.bucketName} is created');
+          bucket.write("Hello world");
+        });
+      }
+    });
+}
 
