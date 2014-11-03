d48ce0fc4d42cc28ed23d239d3a0f82ea507a369
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2014-11-03 22:00:24 +0100

    storage api

--- 2014-11-07/examples/dart/lib/storage.dart
+++ 2014-11-07/examples/dart/lib/storage.dart
@@ -1,23 +1,21 @@
 import 'dart:io';
 import 'package:gcloud/storage.dart';
 
-
-void listBuckets(){
-  String project = Platform.environment["GCLOUD_E2E_TEST_PROJECT"];
-  String serviceKeyLocation = Platform.environment["GCLOUD_E2E_TEST_KEY"];
-  final Storage storage = new Storage(serviceKeyLocation, project);
+void listBuckets(Storage storage, String bucketName){
   storage.listBucketNames().listen(print);
+  storage.bucketExists(bucketName).then((exist){
+    if(!exist) {
+      storage.createBucket(bucketName).then((Bucket bucket) {
+        print('bucket ${bucket.bucketName} is created');
+      });
+    }
+  });
 }
 
-void helloWorldBucket(Storage storage, String bucketName){
-  storage.bucketExists(bucketName).then((exist){
-      if(!exist) {
-        storage.createBucket('myBucket').then((Bucket bucket) {
-          print('bucket ${bucket.bucketName} is created');
-          bucket.writeBytes("HelloWorldDocument", "Hello World".codeUnits).then((ObjectInfo info){
-            print('${info.name} created.  You can dowload it at ${info.downloadLink}}');
-          });
-        });
-      }
+void writeToBucket(Storage storage, String bucketName, String docName){
+  final bucket = storage.bucket(bucketName);
+  bucket.writeBytes(docName, "Hello World".codeUnits).then((ObjectInfo info){
+    print('${info.name} created. Access link: ${info.downloadLink}}');
   });
+  bucket.read(docName).listen(print);
 }
