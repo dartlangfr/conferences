d9341d972f9a524b39a85e56e59b561f2c996cf1
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2014-11-03 21:36:38 +0100

    moar appengine

--- 2014-11-07/examples/dart/bin/server.dart
+++ 2014-11-07/examples/dart/bin/server.dart
@@ -1,7 +1,10 @@
 import 'package:appengine/appengine.dart';
 
 void main(){
+  final appContext = context;
+  final logger = context.services.logging;
   runAppEngine((requestHander) {
+    logger.info("AppEngine started...");
     requestHander.response
       ..write("Hello world")
       ..close();
