e559c6e00375581638f99bef814527cee101259e
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2014-11-03 21:24:42 +0100

    update samples

--- 2014-11-07/examples/dart/lib/datastore.dart
+++ 2014-11-07/examples/dart/lib/datastore.dart
@@ -1,34 +1,33 @@
 import 'package:gcloud/datastore.dart';
-import 'package:gcloud/db.dart';
+import 'package:gcloud/db.dart' as db;
 import 'package:gcloud/common.dart';
 
-@Kind()
-class Person extends Model {
-  @StringProperty(required: true)
+@db.Kind()
+class Person extends db.Model {
+  @db.StringProperty(required: true)
   String name;
 
-  @IntProperty()
+  @db.IntProperty()
   int age;
 
-  @DateTimeProperty()
+  @db.DateTimeProperty()
   DateTime dateOfBirth;
 }
 
 
-void runOperation(final Datastore datastore) {
-  final List<Person> persons  = [
-    new Person()..name = 'John Doe',
-    new Person()..name = 'Jane Doe'
-  ];
+void runOperations(final db.DatastoreDB datastoreDb) {
+  final john = new Person()..name = 'John Doe';
+  final jane = new Person()..name = 'Jane Doe';
+  final List<Person> persons  = [john, jane];
 
-  // TODO fix
-  datastore.commit(inserts: persons).then((_){
+  final List<Entity> entities = persons.map(datastoreDb.modelDB.toDatastoreEntity);
+
+  datastoreDb.datastore.commit(inserts: entities).then((_){
     print('Data inserted');
   });
 
-  //TODO
-  var findQuery = new Query(ancestorKey: personKey, kind: 'Adress');
-  datastore.query(findQuery).then((Page<Entity> page){
+  final findQuery = new Query(ancestorKey: entities.first.key, kind: 'Adress');
+  datastoreDb.datastore.query(findQuery).then((Page<Entity> page){
     print('Found ${page.items.length} items');
   });
 }
